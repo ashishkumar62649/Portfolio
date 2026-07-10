017f890421bcc41b731ad3ce64495c533ae1c364
Ashish Kumar <ashishkumar62649@gmail.com>
2026-07-10 14:20:47 +0530

    Enable Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { inject } from '@vercel/analytics'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 inject()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <SpeedInsights />
     <App />
   </StrictMode>,
 )
